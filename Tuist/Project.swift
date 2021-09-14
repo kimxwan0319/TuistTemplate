28af21d12f959b0bcc04cde77b9cb819e617dbe2
kimxwan <kimxwan0319@naver.com>
2021-09-14 12:55:49 +0900

    📝 :: Project.swift

--- Tuist/Project.swift
+++ Tuist/Project.swift
@@ -1,14 +1,14 @@
 import ProjectDescription
 
 let project = Project(
-    name: "test",
-    organizationName: "com.kimxwan0319",
+    name: "<ProjectName>",
+    organizationName: "<OrganizationName>",
     targets: [
         Target(
-            name: "test",
+            name: "<TargetName>",
             platform: .iOS,
             product: .app,
-            bundleId: "com.kimxwan0319.test",
+            bundleId: "<OrganizationName.TargetName>",
             deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
             infoPlist: .file(path: Path("SupportingFiles/Info.plist")),
             sources: ["Sources/**"],
