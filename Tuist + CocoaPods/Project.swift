7fdc31872ace8f84db386b1bb493c0ffb6845505
kimxwan <kimxwan0319@naver.com>
2021-09-14 13:13:11 +0900

    📑 :: Tuist + CocoaPods

---
+++ Tuist + CocoaPods/Project.swift
@@ -0,0 +1,20 @@
+import ProjectDescription
+
+let project = Project(
+    name: "<ProjectName>",
+    organizationName: "<OrganizationName>",
+    targets: [
+        Target(
+            name: "<TargetName>",
+            platform: .iOS,
+            product: .app,
+            bundleId: "<OrganizationName.TargetName>",
+            deploymentTarget: .iOS(targetVersion: "13.0", devices: .iphone),
+            infoPlist: .file(path: Path("SupportingFiles/Info.plist")),
+            sources: ["Sources/**"],
+            resources: [
+                "Resources/**"
+            ]
+        )
+    ]
+)
